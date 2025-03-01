8ec2d427a5831713d65e57e53a6ae321bf8d553a
Praveen Kumar Dova <praveenkumardova@gmail.com>
2025-03-01 22:50:29 +0000

    terraform p3

--- main.tf
+++ main.tf
@@ -11,3 +11,14 @@ terraform {
 provider "aws" {
   region = "eu-west-2"
 }
+
+resource "aws_instance" "web_server" {
+  #   count         = 2
+  ami           = "ami-0cdb51c8064e24bbc"
+  instance_type = var.instance_type
+
+  tags = {
+    # Name = "web_server-${count.index}"
+    Name = "web_server"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable "instance_type" {
+  description = "AWS instance type"
+  type        = string
+  default     = "t2.micro"
+
+}
