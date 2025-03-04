2a8c358582c974153864742da05afeb8a14f9012
Praveen Kumar Dova <praveenkumardova@gmail.com>
2025-03-04 00:06:14 +0000

    remote s3 backend with multiple statefiles and multiple terraform.tfvars

--- main.tf
+++ main.tf
@@ -6,12 +6,12 @@ terraform {
     }
   }
 
-  # backend "s3" {
-  #   bucket = "tf-state-bkt1"
-  #   key = "terraform.tfstate" #terraform actual state file
-  #   region = "eu-west-2"
-  #   dynamodb_table = "aws-table" #to store lockid's
-  # }
+  backend "s3" {
+    bucket = "tf-state-bkt1"
+    key = "terraform.tfstate" #terraform actual state file
+    region = "eu-west-2"
+    dynamodb_table = "aws-table" #to store lockid's
+  }
 }
 
 # Configure the AWS Provider
